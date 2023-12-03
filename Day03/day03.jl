0e0c355f35957fa7986e4dd7f4e213a9992e6ff4
Tristan Gray-Davies <tgraydavies@gmail.com>
2023-12-03 22:09:35 +0000

    more verbose version of day 3

--- Day03/day03.jl
+++ Day03/day03.jl
@@ -1,16 +1,33 @@
 const DIRECTIONS = CartesianIndices((-1:1,-1:1))
+const Loc = CartesianIndex{2}
 
 function day03(file)
     lines = readlines(file);
     # Uniquely identify each number by its starting position (the key)
-    # Make maps for: position of part of number -> key; key -> number; and symbol position -> symbol,touching keys
-    startpostonum = Dict(CartesianIndex(i,m.offset)=>parse(Int,m.match)
-        for (i,line) in enumerate(lines) for m in eachmatch(r"\d+",line));
-    postostart = Dict(pos=>start for (start,num) in startpostonum for pos in start.+CartesianIndices((0:0,0:(0+length(string(num))-1))));
-    parts = [(symbol=m.match,
-              numstarts=Set(postostart[p] for p in CartesianIndex(i,m.offset).+DIRECTIONS if p in keys(postostart)))
-                for (i,line) in enumerate(lines) for m in eachmatch(r"[^\d\.]",line)];
-    partsum = sum(startpostonum[unqstart] for unqstart in Set(start for part in parts for start in part.numstarts));
-    gearsum = sum(prod(startpostonum[start] for start in part.numstarts) for part in parts if part.symbol=="*" && length(part.numstarts)==2);
+    # Make maps for: position of each integer -> key; key -> number; and symbol position -> symbol,touching keys
+    keytonum    = Dict{Loc,Int}();
+    intloctokey = Dict{Loc,Loc}(); # location of integer to location of start of number
+    for (i,line) in enumerate(lines)
+        for regmatch in eachmatch(r"\d+",line)
+            startposition = Loc(i,regmatch.offset);
+            keytonum[startposition] = parse(Int,regmatch.match);
+            for pos in startposition.+CartesianIndices((0:0,0:(length(regmatch.match)-1)))
+                intloctokey[pos] = startposition;
+            end
+        end
+    end
+    Part = NamedTuple{(:symbol, :numkeys), Tuple{String, Set{Loc}}};
+    parts = Vector{Part}();
+    for (i,line) in enumerate(lines)
+        for regmatch in eachmatch(r"[^\d\.]",line)
+            partposition = Loc(i,regmatch.offset);
+            uniqueadjnumkeys = Set(intloctokey[p] for p in partposition.+DIRECTIONS if p in keys(intloctokey));
+            append!(parts,((symbol=regmatch.match,numkeys=uniqueadjnumkeys),));
+        end
+    end
+    partnumberstarts = Set(start for part in parts for start in part.numkeys); # Set to avoid double-counting matches to multiple parts
+    partsum = sum(keytonum[start] for start in partnumberstarts);
+    gears = [part for part in parts if part.symbol=="*" && length(part.numkeys)==2];
+    gearsum = sum(prod(keytonum[start] for start in gear.numkeys) for gear in gears);
     return partsum,gearsum
 end
